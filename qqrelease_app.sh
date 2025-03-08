0a74ab71821a1d0f70d5d23b0859f95b658d92ec
lrobot <lrobot.qq@gmail.com>
2025-03-08 18:08:26 +0800

    info

--- qqrelease_app.sh
+++ qqrelease_app.sh
@@ -1,8 +1,9 @@
-
+echodo() { echo _omri_run_cmd:"$@"; $@; }
 PODMAN=docker
 which podman > /dev/null 2>&1 && PODMAN=podman
 
 (
   cd app &&
-  ${PODMAN} build -f Dockerfile.qq --tag mediasoupdemo_app:latest .
+  echodo ${PODMAN} build -f Dockerfile.qq --tag mediasoupdemo_app:latest . &&
+  echodo ${PODMAN} push --retry 5 --tls-verify=false mediasoupdemo_app:latest docker://registry.rbat.tk/mediasoupdemo_app:latest
 )
--- qqrelease_server.sh
+++ qqrelease_server.sh
@@ -1,8 +1,10 @@
 
+echodo() { echo _omri_run_cmd:"$@"; $@; }
 PODMAN=docker
 which podman > /dev/null 2>&1 && PODMAN=podman
 
 (
   cd server &&
-  ${PODMAN} build -f Dockerfile.qq --tag mediasoupdemo_server:latest .
+  echodo ${PODMAN} build -f Dockerfile.qq --tag mediasoupdemo_server:latest . &&
+  echodo ${PODMAN} push --retry 5 --tls-verify=false mediasoupdemo_server:latest docker://registry.rbat.tk/mediasoupdemo_server:latest
 )
